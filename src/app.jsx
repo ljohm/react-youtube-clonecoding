ce959a1a9d8c5e3e5ef37a187227c417b04b8cc6
yoon16 <yunn1209@gmail.com>
2021-03-26 21:27:58 +0900

    seaerch_header 구현

--- src/app.jsx
+++ src/app.jsx
@@ -2,9 +2,18 @@ import logo from "./logo.svg";
 import styles from "./app.module.css";
 import React, { useEffect, useState } from "react";
 import VideoList from "./components/video_list/video_list";
+import SearchHeader from "./components/search_header/search_header";
 
 function App({ youtube }) {
   const [videos, setVideos] = useState([]);
+
+  const search = (query) => {
+    youtube //
+      .search(query)
+      .then((result) => {
+        setVideos(result);
+      });
+  };
 
   useEffect(() => {
     youtube
@@ -13,8 +22,11 @@ function App({ youtube }) {
   }, []);
 
   return (
-    <div className={styles.content}>
-      <VideoList videos={videos} />
+    <div>
+      <SearchHeader onSearch={search} />
+      <div className={styles.content}>
+        <VideoList videos={videos} />
+      </div>
     </div>
   );
 }
--- src/components/service/youtube.js
+++ src/components/service/youtube.js
@@ -13,6 +13,21 @@ class Youtube {
     });
     return response.data.items;
   }
+
+  async search(query) {
+    const response = await this.client.get("search", {
+      params: {
+        part: "snippet",
+        maxResults: 25,
+        q: query,
+        type: "video",
+      },
+    });
+    return response.data.items.map((item) => ({
+      ...item,
+      id: item.id.videoId,
+    }));
+  }
 }
 
 export default Youtube;
